2d1f7f698206b2cf6b829a2a1793617cb5b10961
feyre_2001 <kabraniharika7@gmail.com>
2020-09-28 11:15:53 +0530

    #673

---
+++ copy-paste.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h> 
+#include <algorithm>  
+#define ll long long int
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define fr(i,a,b) for(long long int i=a; i<b; i++)
+#define pb push_back
+#define mp make_pair
+#define MOD 1000000007
+using namespace std;
+/*vector<ll>g[1000001];
+ll vis[1000005]={0};
+map<ll,ll>y;
+queue<ll>q ;*/
+ll gcd(ll a, ll b){
+    if(b%a==0) return a;
+    else return gcd(b%a,a);
+}
+ll power(ll a, ll b){
+    ll res=1;
+    while(b)
+    {
+        if(b%2!=0) res=res*a;
+        a=a*a;
+        b=b/2;
+    }
+    return res;
+}
+/*void bfs(ll s)
+{
+       vis[s]=1;
+       q.push(s);
+       ll curr=q.front(); vis[curr]=1;
+       while(!q.empty())
+       {
+              curr=q.front(); q.pop(); // vis[curr]=1;
+              for(ll i=0;i<g[curr].size();i++)
+              {
+                     ll child=g[curr][i];
+                     if(vis[child]==0) 
+                     { 
+                            q.push(child); 
+                            vis[child]=1;
+                     }
+              }
+       }
+}
+ll co=0;
+ll dfs(ll curr)
+{
+       vis[curr]=1;
+       for(ll i=0;i<g[curr].size();i++)
+       {
+              ll child=g[curr][i];
+              if(vis[child]==0)
+              {
+                     co++;
+                     dfs(child);
+              }
+       }
+       return co;
+}*/
+signed main() 
+{
+    fast
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+       ll n,k;
+       cin>>n>>k;
+       ll a[n];
+       ll m=1000005;
+       ll j;
+       for(ll i=0;i<n;i++) {cin>>a[i]; 
+       if(a[i]<=m)
+       {
+              m=a[i];
+              j=i;
+       }
+       }
+       ll ans=0;
+       for(ll i=0;i<n;i++)
+       {
+              if(i!=j) ans=ans+((k-a[i])/m);
+       }
+       cout<<ans<<"\n";
+       
+    }
+}
