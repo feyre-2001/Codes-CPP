859e4b03551d952382ad45fe976875c23df6df03
feyre_2001 <kabraniharika7@gmail.com>
2020-09-29 15:35:31 +0530

    div3

---
+++ symmatrix.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h> 
+#include <algorithm>  
+#define ll long long int
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define fr(i,a,b) for(long long int i=a; i<b; i++)
+#define pb push_back
+#define mp make_pair
+#define MOD 1000000007
+using namespace std;
+/*vector<ll>g[1000001];
+ll vis[1000005]={0};
+map<ll,ll>y;
+queue<ll>q ;*/
+ll gcd(ll a, ll b){
+    if(b%a==0) return a;
+    else return gcd(b%a,a);
+}
+ll power(ll a, ll b){
+    ll res=1;
+    while(b)
+    {
+        if(b%2!=0) res=res*a;
+        a=a*a;
+        b=b/2;
+    }
+    return res;
+}
+/*void bfs(ll s)
+{
+       vis[s]=1;
+       q.push(s);
+       ll curr=q.front(); vis[curr]=1;
+       while(!q.empty())
+       {
+              curr=q.front(); q.pop(); // vis[curr]=1;
+              for(ll i=0;i<g[curr].size();i++)
+              {
+                     ll child=g[curr][i];
+                     if(vis[child]==0) 
+                     { 
+                            q.push(child); 
+                            vis[child]=1;
+                     }
+              }
+       }
+}
+ll co=0;
+ll dfs(ll curr)
+{
+       vis[curr]=1;
+       for(ll i=0;i<g[curr].size();i++)
+       {
+              ll child=g[curr][i];
+              if(vis[child]==0)
+              {
+                     co++;
+                     dfs(child);
+              }
+       }
+       return co;
+}*/
+signed main() 
+{
+    fast
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+       ll n,m;
+       cin>>n>>m;
+       ll c=0;
+       while(n--)
+       {
+              ll a[4];
+              for(ll i=0;i<4;i++) cin>>a[i];
+              if(a[0]!=a[3] && a[1]==a[2]) c=c+1;
+              else if(a[0]==a[3] && a[1]==a[2]) c=c+1;
+              else c=c+0;
+       }
+       if(m%2!=0 || c==0) cout<<"NO\n";
+       else cout<<"YES\n";
+    }
+}
